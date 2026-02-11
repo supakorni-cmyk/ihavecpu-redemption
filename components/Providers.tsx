fb550a3c8ad0115954e9810a67cb57b144679050
supakorni-cmyk <supakorn.i@ihavecpu.com>
2026-02-11 10:35:14 +0700

    first commit

---
+++ components/Providers.tsx
@@ -0,0 +1,8 @@
+// components/Providers.tsx
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
